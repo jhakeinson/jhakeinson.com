fa0393d3e92cc079fe54cf162ac5f66165f1244d
Jhake Inson <me@jhakeinson.com>
2024-10-08 17:41:46 +0800

    UPDATE: update homepage title and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Jhake Inson
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hi, I'm Jhake Inson, a Full Stack Web Developer and GenAI Integrator with over 4 years of experience building powerful, user-friendly web applications. Whether I’m working on back-end logic with NodeJS or enhancing front-end experiences with React and NextJS, I love creating tech that works seamlessly. Lately, I’ve been diving deep into Generative AI features, like RAG and chatbot integration with tools such as Langchain and OpenAI—it's exciting to bring intelligence into my apps! When I’m not coding on my Linux system (always with Neovim open), you’ll probably find me blasting Linkin Park or hanging out with my cats. And while I may be a bit slow on the keyboard due to a neurological condition, my brain works fast, allowing me to solve complex problems with ease and precision, one keystroke at a time.`}
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
