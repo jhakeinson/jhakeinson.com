ab215f09e85ecb0e1cab38540b1d57732ca74a9a
Jhake Inson <me@jhakeinson.com>
2024-10-16 05:37:52 +0800

    feat(blog): create server action for saving blog

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -2,8 +2,12 @@
 
 import { eq } from "drizzle-orm";
 import { db } from "../drizzle";
-import { blog, user } from "../drizzle/schema";
+import { blog } from "../drizzle/schema";
 import { getUserTokens } from "../firebase/utils";
+import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+
+type BlogInsertType = typeof blog.$inferInsert & { authorId?: string | null };
 
 export async function testAction() {
   const tokens = await getUserTokens();
@@ -21,4 +25,28 @@ export async function getBlogBySlug(slug: string) {
   const data = await db.select().from(blog).where(eq(blog.slug, slug));
 
   return data.length === 0 ? null : data[0];
+}
+
+export async function createBlog(newBlog: BlogInsertType) {
+  try {
+    const tokens = await getUserTokens();
+
+    if (!tokens) {
+      throw new Error("unauthorize");
+    }
+
+    const result = await db.insert(blog).values({
+      authorId: tokens.decodedToken.uid,
+      title: newBlog.title,
+      slug: newBlog.slug,
+      content: newBlog.content,
+      published: newBlog.published,
+    });
+
+    revalidatePath("/blog");
+
+    return result.rowCount;
+  } catch (error: unknown) {
+    throw error;
+  }
 }
