cbb365a4fb8eb6284df64e3e012acfaf335c540c
Jhake Inson <me@jhakeinson.com>
2024-10-16 05:45:42 +0800

    chore(ui): update create blog button style and position

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,22 @@
 import { BlogEditorDialog } from "@/components/blog-editor-dialog";
 import { BlogPosts } from "@/components/posts";
+import { getUserTokens } from "@/lib/firebase/utils";
 
 export const metadata = {
   title: "Blog",
   description: "Read my blog.",
 };
 
-export default function Page() {
+export default async function Page() {
+  const tokens = await getUserTokens();
   return (
     <section>
-      <BlogEditorDialog />
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <div className="flex flex-row gap-1">
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+          My Blog
+        </h1>
+        {tokens ? <BlogEditorDialog /> : null}
+      </div>
       <BlogPosts />
     </section>
   );
