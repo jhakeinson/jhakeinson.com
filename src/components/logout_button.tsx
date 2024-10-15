5187e322b39086b43557540e16803c180354c4ca
Jhake Inson <me@jhakeinson.com>
2024-10-16 05:44:15 +0800

    chore(ui): install nzvigation-menu from shadcn

--- src/components/logout_button.tsx
+++ src/components/logout_button.tsx
@@ -5,6 +5,8 @@ import { getAuth, signOut } from "firebase/auth";
 import { app } from "@/lib/firebase";
 import { useAtom } from "jotai";
 import { userAtom } from "@/lib/stores/store";
+import { Button } from "./ui/button";
+import { LogOut, LogOutIcon } from "lucide-react";
 
 export default function LogoutButton() {
   const router = useRouter();
@@ -20,11 +22,9 @@ export default function LogoutButton() {
   }
 
   return user ? (
-    <button
-      onClick={handleLogout}
-      className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
-    >
-      Logout
-    </button>
+    <Button className="text-destructive" onClick={handleLogout} variant="ghost">
+      {"logout"}
+      <LogOut className="h-4 w-4 ml-2" />
+    </Button>
   ) : null;
 }
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,27 +1,34 @@
 import Link from "next/link";
 import { formatDate } from "@/app/blog/utils";
 import { getBlogs } from "@/lib/actions";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export async function BlogPosts() {
   const allBlogs = await getBlogs();
 
   return (
-    <div>
+    <div className="flex flex-col gap-1">
       {allBlogs.map((post) => (
-        <Link
-          key={post.slug}
-          className="flex flex-col space-y-1 mb-4"
-          href={`/blog/${post.slug}`}
-        >
-          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-            <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-              {formatDate(post.createdAt, false)}
-            </p>
-            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-              {post.title}
-            </p>
-          </div>
-        </Link>
+        <Card key={post.slug}>
+          <CardHeader>
+            <div className="flex flex-row justify-between">
+              <CardTitle className="text-lg">
+                <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+              </CardTitle>
+
+              <p className="text-sm flex-shrink-0 ctext-neutral-600 dark:text-neutral-400  tabular-nums">
+                {formatDate(post.createdAt, false)}
+              </p>
+            </div>
+          </CardHeader>
+        </Card>
       ))}
     </div>
   );
